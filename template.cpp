9b0525326452790b2e573980b3466ad485bb04a7
Hasit Bhatt <hasit.p.bhatt@gmail.com>
2016-09-28 19:46:04 +0530

    First Commit

---
+++ template.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define endl "\n"
+#define DFLAG 0
+
+#define DEBUG(x) if(DFLAG) cout << #x << " " << x << endl;
+#define fastin ios::sync_with_stdio(false)
+
+#define REP(i,x) for(int i = 0; i < x; i++)
+#define PER(i,x) for(int i = x-1; i >= 0; i--)
+
+int main() {
+	fastin;
+	return 0;
+}
